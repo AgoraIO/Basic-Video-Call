06909a3bee39768e9327e8bfe4cd407266969699
Hao Yang <tillohao@gmail.com>
2019-07-18 15:18:25 +0800

    <chore> ci templates modified

--- Group-Video/OpenVideoCall-Android/ci.env.py
+++ Group-Video/OpenVideoCall-Android/ci.env.py
@@ -3,26 +3,29 @@
 import re
 import os
 
+
 def main():
-    f = open("./app/build.gradle")
+    f = open("./app/build.gradle", 'r+')
     content = f.read()
     x = re.sub(
         r'(dependencies {$)(.*)', r"\1\n    implementation 'io.agora.rtc:full-sdk:2.4.1'\2", content, flags=re.M)
-    print(x)
-    f.close()
+    f.seek(0)
+    f.write(x)
+    f.truncate()
 
     appId = ""
     if "AGORA_APP_ID" in os.environ:
-      appId = os.environ["AGORA_APP_ID"]
+        appId = os.environ["AGORA_APP_ID"]
     token = ""
 
-    f = open("./app/src/main/res/values/strings_config.xml")
+    f = open("./app/src/main/res/values/strings_config.xml", 'r+')
     content = f.read()
     contentNew = re.sub(r'<#YOUR APP ID#>', appId, content)
     contentNew = re.sub(r'#YOUR ACCESS TOKEN#', token, contentNew)
-    print(contentNew)
-    f.close()
+    f.seek(0)
+    f.write(contentNew)
+    f.truncate()
 
 
 if __name__ == "__main__":
-  main()
+    main()
