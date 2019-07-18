d957adfaa7f2d8b808ebdabe552f9cb4ecb018a8
Hao Yang <tillohao@gmail.com>
2019-07-18 15:54:44 +0800

    <chore> add ci task for Android Tutorial

---
+++ One-to-One-Video/Agora-Android-Tutorial-1to1/ci.env.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import re
+import os
+
+
+def main():
+    f = open("./app/build.gradle", 'r+')
+    content = f.read()
+    x = re.sub(
+        r'(dependencies {$)(.*)', r"\1\n    implementation 'io.agora.rtc:full-sdk:2.4.1'\2", content, flags=re.M)
+    f.seek(0)
+    f.write(x)
+    f.truncate()
+
+    appId = ""
+    if "AGORA_APP_ID" in os.environ:
+        appId = os.environ["AGORA_APP_ID"]
+    token = ""
+
+    f = open("./app/src/main/res/values/strings.xml", 'r+')
+    content = f.read()
+    contentNew = re.sub(r'<#YOUR APP ID#>', appId, content)
+    contentNew = re.sub(r'<#YOUR TOKEN#>', token, contentNew)
+    f.seek(0)
+    f.write(contentNew)
+    f.truncate()
+
+
+if __name__ == "__main__":
+    main()
