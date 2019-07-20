512a46d442a3f3548c379739bec1ffe9f4f9664b
Hai Guo <deliberatekids@gmail.com>
2019-07-20 14:35:49 +0800

    Remove preprocess funtions(C++ layer)

--- Group-Video/OpenVideoCall-Android/app/src/main/cpp/Application.mk
+++
@@ -1,5 +0,0 @@
-STLPORT_FORCE_REBUILD := false
-APP_STL := c++_static
-APP_OPTIM := release
-APP_PLATFORM := android-16
-APP_CPPFLAGS += -std=c++11
--- Group-Video/OpenVideoCall-Android/app/src/main/java/io/agora/propeller/preprocessing/VideoPreProcessing.java
+++
@@ -1,9 +0,0 @@
-package io.agora.propeller.preprocessing;
-
-public class VideoPreProcessing {
-    static {
-        System.loadLibrary("apm-plugin-video-preprocessing");
-    }
-
-    public native void enablePreProcessing(boolean enable);
-}
