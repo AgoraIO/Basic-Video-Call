596ac831cef55bc0472fe87f8268df810e9a18ac
baluoteliz <meixi@live.cn>
2019-10-12 10:50:41 +0800

    update build-release.bat

--- Group-Video/OpenVideoCall-Windows/build_release.bat
+++ Group-Video/OpenVideoCall-Windows/build_release.bat
@@ -1,12 +1,14 @@
 @echo off
 
 title qmake and nmake build prompt
-set VCINSTALLDIR=%1
-set QTDIR=%2
+set VCINSTALLDIR=%~1
+set QTDIR=%~2
 echo vsdir: %VCINSTALLDIR%
 echo qtdir:%QTDIR%
 set PATH=%VCINSTALLDIR%\bin;%QTDIR%\bin;C:\Program Files\7-Zip;%PATH%
 ::shadow build
+set vsdevpath=%VCINSTALLDIR%\vcvarsall.bat
+echo vsdevpath:%vsdevpath%
 call "%VCINSTALLDIR%\vcvarsall.bat"
 qmake OpenVideoCall.pro -spec win32-msvc2013 "CONFIG+=release" "CONFIG+=qml_release"
 nmake
