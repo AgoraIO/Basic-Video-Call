a647bf990a7c4e3085f42d04111ed7b1d797d597
lijuan <2420082764@qq.com>
2020-04-08 19:35:47 +0800

    config eslint

--- One-to-One-Video/Agora-Web-RTS-Tutorial-1to1-Webpack/src/index.js
+++ One-to-One-Video/Agora-Web-RTS-Tutorial-1to1-Webpack/src/index.js
@@ -1,54 +1,54 @@
-import RTCClient from './rtc-client';
-import {getDevices, serializeFormData, validator, resolutions} from './common';
-import "./assets/style.scss";
-import * as M from 'materialize-css';
-import {setFormData, parseFromSearch} from './searchParam';
-import QRCode from 'qrcode';
+import RTCClient from "./rtc-client"
+import {getDevices, Toast, serializeFormData, validator, resolutions} from "./common"
+import "./assets/style.css"
+import * as M from "materialize-css"
+import {setFormData, parseFromSearch} from "./searchParam"
+import QRCode from "qrcode"
 
 if (!AgoraRTS.checkSystemRequirements()) {
-  alert("Your web browser not support AgoraRTS！");
+  alert("Your web browser not support AgoraRTS！")
 } else {
-  console.log("check success");
+  console.log("check success")
 }
 
 $(() => {
 
-  const urlParams = parseFromSearch();
+  const urlParams = parseFromSearch()
 
-  setFormData(urlParams);
+  setFormData(urlParams)
   
   getDevices(function (devices) {
     devices.audios.forEach(function (audio) {
-      $('<option/>', {
+      $("<option/>", {
         value: audio.value,
         text: audio.name,
-      }).appendTo("#microphoneId");
+      }).appendTo("#microphoneId")
     })
     devices.videos.forEach(function (video) {
-      $('<option/>', {
+      $("<option/>", {
         value: video.value,
         text: video.name,
-      }).appendTo("#cameraId");
+      }).appendTo("#cameraId")
     })
     resolutions.forEach(function (resolution) {
-      $('<option/>', {
+      $("<option/>", {
         value: resolution.value,
         text: resolution.name
-      }).appendTo("#cameraResolution");
+      }).appendTo("#cameraResolution")
     })
-    M.AutoInit();
-    M.updateTextFields();
+    M.AutoInit()
+    M.updateTextFields()
   })
 
-  const fields = ['appID', 'channel'];
+  const fields = ["appID", "channel"]
 
-  let rtc = new RTCClient();
+  let rtc = new RTCClient()
 
   $(".autoplay-fallback").on("click", function (e) {
     e.preventDefault()
     const id = e.target.getAttribute("id").split("video_autoplay_")[1]
     console.log("autoplay fallback")
-    if (id === 'local') {
+    if (id === "local") {
       rtc._localStream.resume().then(() => {
         Toast.notice("local resume")
         $(e.target).addClass("hide")
@@ -56,7 +56,7 @@ $(() => {
         Toast.error("resume failed, please open console see more details")
         console.error(err)
       })
-      return;
+      return
     }
     const remoteStream = rtc._remoteStreams.find((item) => `${item.getId()}` == id)
     if (remoteStream) {
@@ -71,42 +71,42 @@ $(() => {
   })
 
   $("#show_profile").on("change", function (e) {
-    e.preventDefault();
-    rtc.setNetworkQualityAndStreamStats(this.checked);
+    e.preventDefault()
+    rtc.setNetworkQualityAndStreamStats(this.checked)
   })
 
   $("#join").on("click", function (e) {
-    e.preventDefault();
+    e.preventDefault()
     console.log("join")
-    const params = serializeFormData();
-    params.audience = urlParams.audience ? urlParams.audience : false;
+    const params = serializeFormData()
+    params.audience = urlParams.audience ? urlParams.audience : false
     if (validator(params, fields)) {
       rtc.join(params).then(() => {
-        rtc.publish();
+        rtc.publish()
       })
     }
   })
 
   $("#leave").on("click", function (e) {
-    e.preventDefault();
+    e.preventDefault()
     console.log("leave")
-    const params = serializeFormData();
+    const params = serializeFormData()
     if (validator(params, fields)) {
-      rtc.leave();
+      rtc.leave()
     }
-  });
+  })
 
   $("#share").on("click", function (e) {
-    e.preventDefault();
+    e.preventDefault()
     if ($("#share_qrcode").hasClass("hide")) {
-      $("#share_qrcode").removeClass("hide");
+      $("#share_qrcode").removeClass("hide")
     }
-    console.log(QRCode);
-    console.log("url: ", window.location.href + "&audience=true");
+    console.log(QRCode)
+    console.log("url: ", window.location.href + "&audience=true")
     QRCode.toCanvas(document.getElementById("qrcode"),
       window.location.href + "&audience=true",
       function (err) {
-        if (err) console.error("generate qrcode failed", err);
+        if (err) console.error("generate qrcode failed", err)
       }
     )
   })
