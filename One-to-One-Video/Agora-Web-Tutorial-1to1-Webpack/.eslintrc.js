af6d86bff1d26580a39ca6b924f71d646fe73049
lijuan <2420082764@qq.com>
2020-04-10 15:20:28 +0800

    fix indent

--- One-to-One-Video/Agora-Web-Tutorial-1to1-Webpack/.eslintrc.js
+++ One-to-One-Video/Agora-Web-Tutorial-1to1-Webpack/.eslintrc.js
@@ -1,23 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jquery": true,
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true,
+        'jquery': true,
     },
-    // "parser": "babel-eslint",
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
     },
-    "rules": {
-        "semi": ["warn", "never"],
-        "quotes": [1, "double"],
-        "no-unused-vars": "warn",
+    'rules': {
+        'indent': [
+            'warn',
+            2
+        ],
+        'linebreak-style': [
+            'warn',
+            'unix'
+        ],
+        'quotes': [
+            'warn',
+            'single'
+        ],
+        'semi': [
+            'warn',
+            'never'
+        ],
+        'no-unused-vars': 0,
     }
-};
+}
