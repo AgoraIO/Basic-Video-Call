458d0314867456531554471bda9aed9e79d38205
CavanSu <403029552@qq.com>
2020-06-02 22:16:31 +0800

    update ios&mac scripts

--- cicd/scripts/sdkdownload.sh
+++ cicd/scripts/sdkdownload.sh
@@ -1,7 +1,5 @@
 TARGET_LIBS_ZIP="agora_sdk.zip"
 TARGET_INTERNAL_FOLDER="agora_sdk"
-TARGET_INTERNAL_STATIC_FOLDER="agora_static_sdk"
-TARGET_INTERNAL_DYNAMIC_FOLDER="agora_dynamic_sdk"
 
 FILE_PATH=`pwd`
 
@@ -9,7 +7,6 @@ echo ${FILE_PATH}
 
 SDK_URL=$1
 COPY_TO=$2
-SDK_TYPE=$3
 
 echo "url" ${SDK_URL}
 
@@ -21,57 +18,19 @@ unzip -q ${TARGET_LIBS_ZIP} -d ${TARGET_INTERNAL_FOLDER}
 
 cd ${TARGET_INTERNAL_FOLDER}
 
-FIRST_UNZIP_FOLDER=""
+FIRST_FOLDER=""
 
 for i in `ls`
 do
-    FIRST_UNZIP_FOLDER=$i
+    FIRST_FOLDER=$i
     echo $i
 done
-
-cd ${FIRST_UNZIP_FOLDER}
-
-DYNAMIC_SDK_TAG="Dynamic"
-DYNAMIC_SDK=""
-STATIC_SDK=""
-
-for i in `ls`
-do
-    if [[ $i =~ ${DYNAMIC_SDK_TAG} ]]
-    then
-    DYNAMIC_SDK=$i
-    echo "Dynamic" $i
-    else
-    STATIC_SDK=$i
-    echo "Static" ${STATIC_SDK}
-    fi
-done
-
-if [[ $SDK_TYPE =~ ${DYNAMIC_SDK_TAG} ]] 
-then
-unzip -q ${STATIC_SDK} -d ${TARGET_INTERNAL_STATIC_FOLDER}
-cd ${TARGET_INTERNAL_STATIC_FOLDER}
-else 
-unzip -q ${DYNAMIC_SDK} -d ${TARGET_INTERNAL_DYNAMIC_FOLDER}
-cd ${TARGET_INTERNAL_DYNAMIC_FOLDER}
-fi
-
-SECOND_UNZIP_FOLDER=""
-
-for i in `ls`
-do
-    SECOND_UNZIP_FOLDER=$i
-    echo ${SECOND_UNZIP_FOLDER}
-done
-
-echo `pwd`
-echo SECOND_UNZIP_FOLDER ${SECOND_UNZIP_FOLDER}
 
 #copy
 DESTINATION=${FILE_PATH}/../../${COPY_TO}
 
 echo DESTINATION ${DESTINATION}
 
-cp -af ${SECOND_UNZIP_FOLDER}/libs/* ${DESTINATION}
+cp -af ${FIRST_FOLDER}/libs/* ${DESTINATION}
 
 echo pwd `pwd`
