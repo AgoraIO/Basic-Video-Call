fb214373c30c64b48e06f54907476b525c65cb99
Qianze Zhang <qianze.zhang@hotmail.com>
2020-11-12 16:58:54 +0800

    Update AppID.m

--- One-to-One-Video/Agora-macOS-Tutorial-Objective-C-1to1/Agora-Mac-Tutorial-Objective-C/AppID.m
+++ One-to-One-Video/Agora-macOS-Tutorial-Objective-C-1to1/Agora-Mac-Tutorial-Objective-C/AppID.m
@@ -14,7 +14,7 @@ NSString *const AppID = <#Your App ID#>;
  * at https://dashboard.agora.io note the token generated are allowed to join corresponding room ONLY.
  */
 /* 如果没有打开鉴权Token, 这里的token值给nil就好
- * 生成Token需要参照官方文档部署Token服务器，开发阶段若想先不部署服务器, 可以在https://dashbaord.agora.io生成
+ * 生成Token需要参照官方文档部署Token服务器，开发阶段若想先不部署服务器, 可以在https://dashboard.agora.io生成
  * 临时Token. 请注意生成Token时指定的频道名, 该Token只允许加入对应的频道
  */
 NSString *const Token = <#Temp Token#>;
